9b1837f41e33fc74e4ef1fb2c0959bce5c887f50
sinisa.miskovic@sattrakt.com <sinisa.miskovic@sattrakt.com>
2024-02-08 11:50:40 +0100

    next init

---
+++ gui/interfaces/interfaces.ts
@@ -0,0 +1,105 @@
+export interface ICharacter {
+  id: number;
+  name: string;
+  description: string;
+  modified: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  resourceURI: string;
+  comics: {
+    available: number;
+    collectionURI: string;
+    items: Array<{
+      resourceURI: string;
+      name: string;
+    }>;
+    returned: number;
+  };
+  series: {
+    available: number;
+    collectionURI: string;
+    items: Array<{
+      resourceURI: string;
+      name: string;
+    }>;
+    returned: number;
+  };
+  stories: {
+    available: number;
+    collectionURI: string;
+    items: Array<{
+      resourceURI: string;
+      name: string;
+      type: string;
+    }>;
+    returned: number;
+  };
+  events: {
+    available: number;
+    collectionURI: string;
+    items: Array<any>;
+    returned: number;
+  };
+  urls: Array<{
+    type: string;
+    url: string;
+  }>;
+}
+
+export interface MarvelOptions {
+  offset?: number | string;
+  limit?: number | string;
+}
+
+export interface Comic {
+  id: number;
+  digitalId: number;
+  title: string;
+  issueNumber: number;
+  variantDescription: string;
+  description: string;
+  modified: string;
+  isbn: string;
+  upc: string;
+  diamondCode: string;
+  ean: string;
+  issn: string;
+  format: string;
+  pageCount: number;
+  resourceURI: string;
+  urls: { type: string; url: string }[];
+  series: { resourceURI: string; name: string };
+  variants: { resourceURI: string; name: string }[];
+  collections: any[];
+  collectedIssues: any[];
+  dates: { type: string; date: string }[];
+  prices: { type: string; price: number }[];
+  thumbnail: { path: string; extension: string };
+  images: any[];
+  creators: {
+    available: number;
+    collectionURI: string;
+    items: any[];
+    returned: number;
+  };
+  characters: {
+    available: number;
+    collectionURI: string;
+    items: any[];
+    returned: number;
+  };
+  stories: {
+    available: number;
+    collectionURI: string;
+    items: any[];
+    returned: number;
+  };
+  events: {
+    available: number;
+    collectionURI: string;
+    items: any[];
+    returned: number;
+  };
+}
---
+++ gui/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env:{
+        API_HOST:'http://localhost:3001'
+    }
+};
+
+export default nextConfig;
